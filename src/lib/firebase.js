38d97af54fe317b9c12671b7c5c1442a8ee989ca
Md Shoaib Hossain <shoaibhossain188@gmail.com>
2026-06-12 21:47:23 +0600

    secure my firebase api key

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2kLco6C9LBKwkFkmPnNws23xmx6kmaAs",
-  authDomain: "shop-zone-26ae9.firebaseapp.com",
-  projectId: "shop-zone-26ae9",
-  storageBucket: "shop-zone-26ae9.firebasestorage.app",
-  messagingSenderId: "914240024270",
-  appId: "1:914240024270:web:6c24597724281269e72a9f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
